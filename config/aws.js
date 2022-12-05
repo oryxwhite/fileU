725b8b0e328dea5746a02426077ea065ed85eba7
oryx <mtxymtxy@gmail.com>
2022-12-05 17:12:40 -0500

    edit s3 creds to cyclic bucket

--- config/aws.js
+++ config/aws.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const path = require('path')
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    // accessKeyId: process.env.AWS_ACCESS_KEY,
+    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
 // const AWSupload = async (filename, file) => {
